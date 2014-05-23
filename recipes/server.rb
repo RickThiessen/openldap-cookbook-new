ca893263a1bb41ae34deee79b22904ecb5a3bbaa
Donavan Pantke <dpantke@appriss.com>
2014-05-23 10:26:33 -0400

    Add configuration recipe.

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,3 +1,25 @@
-template "/tmp/configmaindb.ldif" do
+template "/tmp/ldapconfig.ldif" do
 	   source "slapd-config.ldif.erb"
+end
+
+
+execute "slapdlog" do
+	command "echo \"local4.*\     /var/log/openldap.log\" >/etc/rsyslog.d/ldap.conf"
+end
+
+service "rsyslog" do
+	action :restart
+end
+
+service "slapd" do
+	action [ :enable, :start ]
+end
+
+
+execute "configure ldap" do 
+	command "sleep 5;ldapmodify -Y EXTERNAL -H ldapi:/// </tmp/configmaindb.ldif"
+end
+
+execute "logrotate_ldap" do
+	command "sed -i '1i/var/log/openldap.log' /etc/logrotate.d/syslog"
 end
